4ee9f0c324a6b1f14e64c85910765d021f743966
Noles <noles@a3k.net>
2018-03-16 00:32:52 +0100

    Swap to bootstrap - login and base layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Solder')</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    dd
 
     <!-- @TODO really needed? -->
     <script>
@@ -24,47 +23,20 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    @if (Auth::guest())
-                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                <a href="{{ route('logout') }}" class="dropdown-item"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </div>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-
+    
+    <div class="row">
+        <div class="col-1 pl-0 bg-primary">
+            @include('partials.directory')
         </div>
-    </nav>
-
-    @yield('content')
+        <div class="col-11 pl-0">
+            @include('partials.navigation')
+    
+            <div class="container">             
+                @yield('content')
+            </div>
+       </div>
+    </div>
+    
 </div>
 
     <!-- Scripts -->
--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,21 +1,29 @@
-<nav class="navbar border-bottom-grey-light" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" role="navigation" aria-label="main navigation">
+    <a class="navbar-brand" href="#">
         &nbsp;
-    </div>
-    <div class="navbar-menu">
-        <div class="navbar-end">
-            <a class="navbar-item" href="/library"> Library </a>
-            <a class="navbar-item" href="/settings/api"> Settings </a>
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link"> {{ Auth()->user()->username }} </a>
-                <div class="navbar-dropdown is-right is-radiusless">
-                    <a class="navbar-item" href="javascript:{}" onclick="document.getElementById('logout').submit(); return false;"> Log Out </a>
-
-                    <form id="logout" action="{{ route('auth.logout') }}" method="POST">
-                        {{ csrf_field() }}
-                    </form>
-                </div>
-            </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/library">Library</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/settings/api">Settings</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUser" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{ Auth()->user()->username }}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownUser">
+          <a class="dropdown-item" href="javascript:{}" onclick="document.getElementById('logout').submit(); return false;">Log Out</a>
+          <form id="logout" action="{{ route('auth.logout') }}" method="POST">
+            {{ csrf_field() }}
+          </form>
         </div>
+      </li>
+      </ul>
     </div>
 </nav>
